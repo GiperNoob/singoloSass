4b2d6bd98901494a8a9a759c57ebe2f4ad5e010a
Nikolai Pribitkov <pribitkov2013@mail.ru>
2020-05-04 20:27:33 +0300

    docs: create function filterBySelectedTag

--- js/script.js
+++ js/script.js
@@ -1,31 +1,52 @@
-
 const menu = document.getElementById('menu');
 menu.addEventListener('click', (event) => {
     menu.querySelectorAll('a').forEach(el => el.classList.remove('navigation__link_active'));
     event.target.classList.add('navigation__link_active');
 })
 
-const galeryButtons = document.getElementById('galery');
-galeryButtons.addEventListener('click', (event) => {
-    galeryButtons.querySelectorAll('button').forEach(el => el.classList.remove('portfolio-nav__link_active'));
-    event.target.classList.add('portfolio-nav__link_active');
-})
-
-const galeryPictures = document.getElementById('gallery-list');
-galeryPictures.addEventListener('click', (event) => {
-    galeryPictures.querySelectorAll('img').forEach(el => el.classList.remove('gallery-list__ico_active'));
+const galleryPictures = document.getElementById('gallery-list');
+galleryPictures.addEventListener('click', (event) => {
+    galleryPictures.querySelectorAll('img').forEach(el => el.classList.remove('gallery-list__ico_active'));
     event.target.classList.add('gallery-list__ico_active');
 })
 
-let screenH = document.getElementById('screenH')
-let buttonH = document.querySelector('#circleH');
-buttonH.onclick = function() {
-    screenH.style.display = (screenH.style.display == 'block') ? 'none' : 'block';
+const galleryButtons = document.getElementById('gallery');
+galleryButtons.addEventListener('click', (event) => {
+    galleryButtons.querySelectorAll('button').forEach(el => el.classList.remove('portfolio-nav__link_active'));
+    event.target.classList.add('portfolio-nav__link_active');
+
+    if (event.target.dataset.type == 'All') {
+        showAllPictures();
+    } else {
+        filterBySelectTag(event.target.dataset.type);
+    }
+})
+
+const showAllPictures = () => {
+    galleryPictures.querySelectorAll('li').forEach(picture => {
+        picture.classList.remove('hidden');
+    })
+}
+
+const filterBySelectTag = (event) => {
+    galleryPictures.querySelectorAll('li').forEach(picture => {
+        if (picture.dataset.type == event) {
+            picture.classList.remove('hidden');
+        } else {
+            picture.classList.add('hidden');
+        }
+    })
+}
+
+let screenHorizontal = document.getElementById('screenH')
+let buttonHorizontal = document.getElementById('circleH');
+buttonHorizontal.onclick = function () {
+    screenHorizontal.style.display = (screenHorizontal.style.display == 'block') ? 'none' : 'block';
 
 }
 
-let screenV = document.getElementById('screenV');
-let buttonV = document.querySelector('#circleV');
-buttonV.onclick = function() {
-    screenV.style.display = (screenV.style.display == 'block') ? 'none' : 'block';
+let screenVertical = document.getElementById('screenV');
+let buttonVertical = document.getElementById('circleV');
+buttonVertical.onclick = function () {
+    screenVertical.style.display = (screenVertical.style.display == 'block') ? 'none' : 'block';
 }
